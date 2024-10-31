321f41bf548a962676102b431345cae0c42a57d3
thedatanotebook <wheresthecode@gmail.com>
2024-10-31 07:05:22 -0700

    add timer_decorator

---
+++ Arrays.py
@@ -0,0 +1,5 @@
+from timer_decorator import get_time
+
+@get_time
+def contains_duplicate():
+    pass
---
+++ timer_decorator.py
@@ -0,0 +1,45 @@
+import time 
+from functools import wraps
+
+def get_time(function):
+    @wraps(function)
+    def wrapper(*args,**kwargs):
+        start_time = time.time()
+        result = function(*args, **kwargs)
+        end_time = time.time()
+        duration = ((end_time-start_time)*10000)
+        print(f"Function '{function.__name__}' took {duration:.4f} seconds to complete")
+        return result
+    return wrapper
+
+
+@get_time
+def linear_search_for_loop(arr, target):
+    for i in range(len(arr)):
+        if arr[i]==target:
+            return i
+    return -1
+
+@get_time
+def linear_search_w_enumerate(arr,target):
+    for i, value in enumerate(arr):
+        if value == target:
+            return i 
+    return -1
+
+
+array = [1,2,3,4,5,6,7,8,9,10]
+target = 7
+
+
+print(linear_search_for_loop(array, 7))
+print(linear_search_w_enumerate(array, 7))
+
+#Results
+
+# Function 'linear_search_for_loop' took 0.0191 seconds to complete
+# Function 'linear_search_w_enumerate' took 0.0095 seconds to complete
+
+
+
+
